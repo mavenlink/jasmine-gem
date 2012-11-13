d416590b8fe0dc90506cfefa5a89d2b573a90ace
Reid Gillette <reid@mavenlink.com>
2012-11-13 11:14:25 -0800

    require action_view/base so that asset path helpers work in the Rails asset pipeline

--- lib/jasmine/application.rb
+++ lib/jasmine/application.rb
@@ -19,6 +19,15 @@ module Jasmine
         use Rack::Jasmine::CacheControl
         if Jasmine::Dependencies.rails_3_asset_pipeline?
           map('/assets') do
+            # In order to have asset helpers like asset_path and image_path, we need to require 'action_view/base'.  This
+            # triggers run_load_hooks on action_view which, in turn, causes sprockets/railtie to load the Sprockets asset
+            # helpers.  Alternatively, you can include the helpers yourself without loading action_view/base:
+            #   Rails.application.assets.context_class.instance_eval do
+            #     include ::Sprockets::Helpers::IsolatedHelper
+            #     include ::Sprockets::Helpers::RailsHelper
+            #   end
+            require 'action_view/base'
+
             run Rails.application.assets
           end
         end
